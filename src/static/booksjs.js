2edba6c80be2e6390e221a17f23c174a000fa23e
TobKed <tobiaszkedzierski@gmail.com>
2018-10-30 21:03:03 +0100

    make table clickable with changed cursor

--- src/static/booksjs.js
+++ src/static/booksjs.js
@@ -1,17 +1,34 @@
 $(document).ready(function () {
 
-        $.ajax(
-            {
-                url: 'book/',
-                data: {},
-                type: "GET",
-                dataType: "json",
-                success:    booksSucces,
-                error:      function () { console.log("error") },
-                complete:   function () { console.log("complete") }
-            });
+    loadBooks();
 
 });
+
+
+function loadBooks() {
+    $.ajax(
+    {
+        url: 'books/',
+        data: {},
+        type: "GET",
+        dataType: "json",
+        success:    booksSucces,
+        error:      function () {
+            console.log("error");
+            errorLoadingBooks();
+        },
+        complete:   function () {
+            console.log("complete");
+            makeTableClickable();
+        }
+    });
+}
+
+function errorLoadingBooks() {
+    $('#booksTable > tbody').append("<tr>" +
+                    "<td colspan=2 class='text-center'> Error loading books </td>" +
+                "</tr>")
+}
 
 function booksSucces(data) {
     console.log(data[0]);
@@ -20,7 +37,7 @@ function booksSucces(data) {
 
     $.each(data, function (key, val) {
         let titleAuthorRow = $(
-            "<tr data-bookId=" + val.id + ">" +
+            "<tr class='clickable-row' data-bookId=" + val.id + ">" +
                 "<td>" + val.author + "</td>" +
                 "<td>" + val.title + "</td>" +
             "</tr>"
@@ -42,3 +59,15 @@ function booksSucces(data) {
     });
 
 };
+
+function makeTableClickable() {
+    $(".clickable-row").click(function() {
+        $(this).next().toggleClass("d-none");
+    });
+
+    addPointerToRow();
+}
+
+function addPointerToRow() {
+    $(".clickable-row").css( 'cursor', 'pointer' )
+}
