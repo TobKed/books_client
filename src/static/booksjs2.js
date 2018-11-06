6ea74b044c77552fe92a94f42676b9b4febc5ab0
TobKed <tobiaszkedzierski@gmail.com>
2018-11-06 20:04:00 +0100

    add booksjs2.js

---
+++ src/static/booksjs2.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    console.log("test");
+});
+
+let genres = {
+    1: "Romans",
+    2: "Obyczajowa",
+    3: "Sci-fi i fantasy",
+    4: "Literatura faktu",
+    5: "Popularnonaukowa",
+    6: "Poradnik",
+    7: "Kryminał, sensacja"
+};
