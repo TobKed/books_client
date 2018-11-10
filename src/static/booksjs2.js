3e6cae88a54c77cf566bf946e4899b9d8055fff9
TobKed <tobiaszkedzierski@gmail.com>
2018-11-10 18:31:54 +0100

    add ajaxCall func

--- src/static/booksjs2.js
+++ src/static/booksjs2.js
@@ -2,7 +2,10 @@ $(document).ready(function () {
     console.log("test");
 });
 
-let genres = {
+let DEBUG = true;
+let ENDPOINTURL = "http://localhost:8000/book/";
+
+let GENRES = {
     1: "Romans",
     2: "Obyczajowa",
     3: "Sci-fi i fantasy",
@@ -11,3 +14,33 @@ let genres = {
     6: "Poradnik",
     7: "Kryminał, sensacja"
 };
+
+function ajaxCall({bookId = "",
+                  type = "GET",
+                  dataType ="json",
+                  data = {},
+                  success=null, error=null, complete=null} = {}) {
+
+    let ajaxSettings = {
+        url: ENDPOINTURL + bookId,
+        data: data,
+        type: type,
+        success: function (data) {
+            if (DEBUG) console.log("success - ajaxCall()");
+            if (DEBUG) console.log(data);
+            if (success) success(data);
+        },
+        error: function (data) {
+            if (DEBUG) console.log("error - ajaxCall()");
+            if (DEBUG) console.log(data);
+            if (error) error(data);
+        },
+        complete: function () {
+            if (DEBUG) console.log("complete - ajaxCall()");
+            if (complete) complete();
+        }
+    };
+    if ((dataType != null) && (dataType !== undefined)) ajaxSettings.dataType = dataType;
+
+    $.ajax(ajaxSettings);
+}
